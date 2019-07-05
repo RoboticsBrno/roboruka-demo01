213b70ed7c38ace83fafb5f779f94db3b4c558e6
Vojtech Bocek <vbocek@gmail.com>
2019-07-05 10:56:56 +0200

    Try to fix tests

--- pre_extra_script.py
+++ pre_extra_script.py
@@ -3,7 +3,22 @@ import shutil
 
 Import("env")
 
+def copy_for_tests(cfg_path):
+    with open(cfg_path, "wb") as fout:
+        with open(cfg_path + ".default", "rb") as fin:
+            for l in fin:
+                if l.startswith("#define OWNER "):
+                    fout.write('#define OWNER "Tests"\n')
+                elif l.startswith("#define NAME "):
+                    fout.write('#define NAME "TestsName"\n')
+                else:
+                    fout.write(l)
+
 cfg_path = os.path.join(env["PROJECTSRC_DIR"], "config.hpp")
 if not os.path.isfile(cfg_path):
-    shutil.copyfile(cfg_path + ".default", cfg_path)
-    print("Generated default config at %s!" % cfg_path)
+    if os.environ.get("ROBORUKA_TESTS", "") != "1":
+        shutil.copyfile(cfg_path + ".default", cfg_path)
+        print("Generated default config at %s!" % cfg_path)
+    else:
+        copy_for_tests(cfg_path)
+        print("Generated config for tests at %s!" % cfg_path)
